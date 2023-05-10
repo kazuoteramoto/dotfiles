41884f21b28dea2fb4fe8dec588dd13e825a6bdc
Kazuo Teramoto <kaz.rag@gmail.com>
2023-05-10 12:38:41 -0300

    fix(nvim): minor updates

--- dot_config/nvim/lua/plugins/override.lua
+++ dot_config/nvim/lua/plugins/override.lua
@@ -1,23 +1,16 @@
 return {
   {
-    "nvim-treesitter/nvim-treesitter",
-    opts = {
-      indent = {
-        enable = true,
-        disable = { "python" },
-      },
-    },
-  },
-  {
     "jose-elias-alvarez/null-ls.nvim",
     opts = function()
       local null_ls = require("null-ls")
       return {
         sources = {
-          -- null_ls.builtins.formatting.prettierd,
           null_ls.builtins.diagnostics.ruff,
           null_ls.builtins.formatting.stylua,
+          null_ls.builtins.formatting.isort,
           null_ls.builtins.formatting.yapf,
+          null_ls.builtins.formatting.jq,
+          null_ls.builtins.completion.spell,
         },
       }
     end,
@@ -34,9 +27,26 @@ return {
   {
     "neovim/nvim-lspconfig",
     opts = {
-      diagnostics = {
+      diagnostics = { -- make diagnostics less intrusive
         underline = false,
         virtual_text = false,
+      },
+      format = {
+        timeout_ms = 2000,
+      },
+      servers = {
+        pyright = {
+          settings = {
+            python = {
+              analysis = {
+                autoSearchPaths = true,
+                useLibraryCodeForTypes = true,
+                typeCheckingMode = "off", -- improve performance
+                diagnosticMode = "openFilesOnly", -- improve performance
+              },
+            },
+          },
+        },
       },
     },
   },
@@ -54,25 +64,13 @@ return {
   ]]
 
       dashboard.section.header.val = vim.split(logo, "\n")
-      dashboard.section.buttons.val = {
-        dashboard.button("f", " " .. " Find file", ":Telescope find_files <CR>"),
-        dashboard.button("n", " " .. " New file", ":ene <BAR> startinsert <CR>"),
-        dashboard.button("r", " " .. " Recent files", ":Telescope oldfiles <CR>"),
-        dashboard.button("g", " " .. " Find text", ":Telescope live_grep <CR>"),
-        dashboard.button("c", " " .. " Config", ":e $MYVIMRC <CR>"),
-        dashboard.button("s", "勒" .. " Restore Session", [[:lua require("persistence").load() <cr>]]),
-        dashboard.button("l", "鈴" .. " Lazy", ":Lazy<CR>"),
-        dashboard.button("q", " " .. " Quit", ":qa<CR>"),
-      }
-      for _, button in ipairs(dashboard.section.buttons.val) do
-        button.opts.hl = "AlphaButtons"
-        button.opts.hl_shortcut = "AlphaShortcut"
-      end
-      dashboard.section.footer.opts.hl = "Type"
-      dashboard.section.header.opts.hl = "AlphaHeader"
-      dashboard.section.buttons.opts.hl = "AlphaButtons"
-      dashboard.opts.layout[1].val = 8
       return dashboard
     end,
+  },
+  { -- reduce notify verbosity
+    "rcarriga/nvim-notify",
+    opts = {
+      level = "WARN",
+    },
   },
 }
--- dot_config/nvim/lua/plugins/plugins.lua
+++ dot_config/nvim/lua/plugins/plugins.lua
@@ -1,37 +1,45 @@
 return {
-  {
+  { -- colorscheme
     "sainnhe/gruvbox-material",
     init = function()
       vim.g.gruvbox_material_background = "soft"
       vim.g.gruvbox_material_better_performance = 1
     end,
   },
-  {
+  { -- colorscheme
     "LazyVim/LazyVim",
     opts = {
       colorscheme = "gruvbox-material",
     },
   },
-  {
+  { -- git diff
     "sindrets/diffview.nvim",
     dependencies = { "nvim-lua/plenary.nvim" },
     keys = { { "<leader>gd", "<cmd>DiffviewOpen<cr>", desc = "Diffview" } },
     cmd = "DiffviewOpen",
   },
-  {
+  { -- rainbow () [] {}
     "mrjones2014/nvim-ts-rainbow",
     config = function()
-      require("nvim-treesitter.configs").setup({ rainbow = {
-        enable = true,
-      } })
+      require("nvim-treesitter.configs").setup({
+        rainbow = {
+          enable = true,
+        },
+      })
     end,
     dependencies = { "nvim-treesitter/nvim-treesitter" },
   },
-  {
+  { -- code outline
     "stevearc/aerial.nvim",
     config = function()
       require("aerial").setup()
     end,
-    keys = { { "<leader>ct", "<cmd>AerialToggle left<cr>", desc = "Aerial" } },
+    keys = {
+      {
+        "<leader>ct",
+        "<cmd>AerialToggle left<cr>",
+        desc = "Aerial (Code Outline)",
+      },
+    },
   },
 }
